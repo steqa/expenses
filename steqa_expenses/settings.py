3eb86d8e8fad346bebbab04993039d519476e8db
steqa <5.steqa@gmail.com>
2022-08-11 19:25:43 +0700

    0.7

--- steqa_expenses/settings.py
+++ steqa_expenses/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'widget_tweaks',
-    
+        
     'authentication.apps.AuthenticationConfig',
     'expenses.apps.ExpensesConfig',
 ]

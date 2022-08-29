8c611c7ad0acd812fe659340c5f8f150b9964001
steqa <5.steqa@gmail.com>
2022-08-29 16:01:40 +0700

    deploy

--- steqa_expenses/settings.py
+++ steqa_expenses/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g20*-354k3nr=j%lbj+=9dy^ln+__n^m99c=okizkvcrtk&q^r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['steqa-expenses.herokuapp.com', 'steqa.ga']
 
 
 # Application definition

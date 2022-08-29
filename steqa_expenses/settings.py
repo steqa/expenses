aaedb4a523c84042c651fe94f295cda5eac2fc41
steqa <5.steqa@gmail.com>
2022-08-29 16:44:25 +0700

    deploy

--- steqa_expenses/settings.py
+++ steqa_expenses/settings.py
@@ -125,9 +125,7 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
